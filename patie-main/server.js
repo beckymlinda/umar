4e4aedb27a2bd7265409b0a0fd2766af3e77f64e
Rabecca Mulinda <beckymlinda@gmail.com>
2025-02-14 08:07:38 +0200

    js

--- patie-main/server.js
+++ patie-main/server.js
@@ -1,16 +1,14 @@
 const express = require("express");
-const path = require("path");
-
 const app = express();
+
 const PORT = process.env.PORT || 3000;
 
-// Serve static files from "public" folder
-app.use(express.static(path.join(__dirname, "public")));
-
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+// Define a simple route
+app.get("/", (req, res) => {
+  res.send("Hello, Render!");
 });
 
+// Start the server
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
